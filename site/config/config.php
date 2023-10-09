c3b84874c3294dbb8e1c02742556922486bc59a1
Sunnyka <71286632+Sunnyka98@users.noreply.github.com>
2023-10-09 11:35:13 +0200

    minimal example for overview pages

--- site/config/config.php
+++ site/config/config.php
@@ -5,28 +5,4 @@
     ],
     'languages' => true,
     'language.detect' => true,
-    'routes' => [
-        [
-            'pattern' => 'de/feed',
-            'language' => '*',
-            'action'  => function () {
-                $options = [
-                    'title' => 'Dokumentation',
-                    'sort' => false,
-                ];
-                return site()->homePage()->index()->feed($options);
-            }
-        ],
-        [
-            'pattern' => 'en/feed',
-            'language' => '*',
-            'action'  => function () {
-                $options = [
-                    'title' => 'Documentation',
-                    'sort' => false,
-                ];
-                return site()->homePage()->index()->feed($options);
-            }
-        ]
-    ]
 ];
--- site/templates/default.php
+++ site/templates/default.php
@@ -1,8 +1,69 @@
-<?= $page->title() ?>
-<?= $page->text()->kt() ?>
-
 <?php 
-    $children = $page->children();
-    foreach ($children as $child) {
-        echo ("<a href='".$child->uri()."'>".$child->title()."</a>\n");
+    if (!function_exists("getOverviewTable")) {
+        function getOverviewTable($page, $depth)
+        {
+            $result = "";
+            $pages = false;
+            if ($page->hasChildren()) {
+                $pages = $page->children();
+            }
+            if (isset($pages) && $pages != false) {
+                if ($depth == 2) {
+                    $result .= "<table>";
+                }
+                foreach ($pages as $page) {
+                    switch ($depth) {
+                        case 3:
+                            $result .= "<h3><a href='" . $page->url() . "/'>" . $page->title() . "</a></h3>\n";
+                            $result .= "<p><b>" . $page->text() . "</b></p>\n";
+                            $result .= getOverviewTable($page, $depth - 1);
+                            break;
+                        case 2:
+                            $hasChild = getOverviewTable($page, $depth - 1) != "";
+                            $result .= "<tr>\n";
+                            $result .= "<td><a href='" . $page->url() . "/'>" . $page->title() . "</a></td>\n";
+                            $result .= "<td>" . $page->text() . "</td>\n";
+                            $result .= "</tr>\n";
+                            if ($hasChild) {
+                                $result .= "<tr>\n";
+                                $result .= "<td>\n";
+                                $result .= "<table>\n";
+                                $result .= getOverviewTable($page, $depth - 1);
+                                $result .= "</table>\n";
+                                $result .= "</td>\n";
+                                $result .= "</tr>\n";
+                            }
+                            break;
+                        case 1:
+                            $result .= "<tr>\n";
+                            $result .= "<td><a href='" . $page->url() . "/'>" . $page->title() . "</a></td>\n";
+                            $result .= "<td>" . $page->text() . "</td>\n";
+                            $result .= "</tr>\n";
+                            break;
+                    }
+                }
+                if ($depth == 2) {
+                    $result .= "</table>\n";
+                }
+            }
+            return $result;
+        }
+    } 
+?>
+
+<style>
+    table, tr,td{
+        border: thin solid;
     }
+</style>
+<?php
+    echo $page->text()->kt();
+        //depth = 3 if the children is overwiev
+    $pages = $page->children();
+    if ($pages != false) {
+        echo getOverviewTable($page, 3);
+    }
+?>
+
+
+    
