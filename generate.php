d51814766109ca02d70a75d1e14e27f36aa9c3be
Sunnyka <71286632+Sunnyka98@users.noreply.github.com>
2023-10-09 12:42:07 +0200

    :broom: tidy up generate.php

--- generate.php
+++ generate.php
@@ -4,17 +4,6 @@ require __DIR__ . '/kirby/bootstrap.php';
 
 $kirby = kirby();
 $staticSiteGenerator = new D4L\StaticSiteGenerator($kirby);
-$staticSiteGenerator->setCustomRoutes([
-    [
-        'path' => 'feed-de.xml',
-        'route' => 'de/feed'
-    ],
-    [
-        'path' => 'feed-en.xml',
-        'route' => 'en/feed'
-    ],
-]);
-$staticSiteGenerator->setIgnoreUntranslatedPages(true);
 $files = $staticSiteGenerator->generate('./build');
 
 // For debugging we want to display which pages were rendered
