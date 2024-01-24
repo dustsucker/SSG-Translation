5ab2c496d85434d7e47433a2d8288f0ac94f9c0f
Sunnyka <71286632+Sunnyka98@users.noreply.github.com>
2024-01-24 14:53:23 +0100

    clean up

--- site/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php return[
-    'debug' => true,
-    'panel' => [
-        'install' => true 
-    ],
-    'languages' => true,
-    'language.detect' => true,
-];
--- site/languages/de.php
+++
@@ -1,9 +0,0 @@
-<?php 
-return [
-  'code' => 'de',
-  'default' => true,
-  'direction' => 'ltr',
-  'locale' => 'de_DE',
-  'name' => 'Deutsch',
-  'translations' => []
-];
